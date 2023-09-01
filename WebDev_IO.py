c9aea4c93d980525c69934d0a26efdd3fa5265e4
AkbarMuh <56223880+AkbarMuh@users.noreply.github.com>
2023-09-01 22:24:36 +0700

    Update WebDev_IO.py

--- WebDev_IO.py
+++ WebDev_IO.py
@@ -543,7 +543,7 @@ if menu_selected == 'Chat':
 			#from elevenlabs import clone, generate, play, set_api_key, save
 			#from elevenlabs.api import History
 
-			set_api_key("6b5b2cfbb952aff99001c460c4355b5c")
+			#set_api_key("6b5b2cfbb952aff99001c460c4355b5c")
 
 			# voice = clone(
 			#     name="Voice Name",
